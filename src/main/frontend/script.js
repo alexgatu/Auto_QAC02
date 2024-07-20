5f4ddb338cda34f68211ba2a426784a53f10f839
florin.barbuceanu <florin.barbuceanu@ihsmarkit.com>
2024-07-20 13:46:47 +0300

    Add HTML,css, js

---
+++ src/main/frontend/script.js
@@ -0,0 +1,34 @@
+function average(a,b){
+    return (a+b)/2;
+}
+
+function showAlert(){
+    window.alert("This is an alert js function");
+}
+
+function displayFullName(){
+    var firstName = document.getElementById("firstNameValue").value;
+    var lastName = document.getElementById("lastNameValue").value;
+    var fullName="";
+    var fullNameElement=  document.getElementById("fullName");
+
+
+
+    fullNameElement.innerHTML="";
+
+    if(firstName === "" && lastName === ""){
+        window.alert("Please enter both first name and last name");
+       }
+    else if(firstName === "" ){
+        window.alert("Please enter first name");
+        }
+    else if(lastName === "" ){
+        window.alert("Please enter last name");
+    } else {
+       fullNameElement.innerHTML = upperCaseFirstLetter(firstName) + " " + upperCaseFirstLetter(lastName);
+    }
+}
+
+function upperCaseFirstLetter(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
