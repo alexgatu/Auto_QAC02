497d0b0c7ef0ceb8215b12360c0e08ee2a78f9e6
alexgatu <alexandru.gatu@gmail.com>
2024-08-10 10:42:41 +0300

    added Encapsulation, lombok

---
+++ src/main/java/courses/java/oop2/Main.java
@@ -0,0 +1,10 @@
+package courses.java.oop2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Shape s1 = new Shape("fuchsia");
+        s1.setColor("red");
+        System.out.println("The color of the shape is " + s1.getColor());
+    }
+}
---
+++ src/main/java/courses/java/oop2/Shape.java
@@ -0,0 +1,11 @@
+package courses.java.oop2;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Shape {
+    @Getter @Setter
+    private String color;
+
+}
